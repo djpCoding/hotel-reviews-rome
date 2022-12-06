098d0c7d582e62d13f35295b898a0f387d3abdff
djpCoding <119716129+djpCoding@users.noreply.github.com>
2022-12-06 16:17:42 -0600

    Update hotel-review-rome-code.py

--- hotel-review-rome-code.py
+++ hotel-review-rome-code.py
@@ -15,8 +15,7 @@ from heapq import nlargest
 from PIL import Image
 import os
 import spacy
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+nlp = spacy.load("en_core_web_sm")
 from spacy import displacy
 stopwords=list(STOP_WORDS)
 from string import punctuation
