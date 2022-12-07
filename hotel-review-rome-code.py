a61da0dad56ace769b4fc500002f9a74b00cbaa8
djpCoding <119716129+djpCoding@users.noreply.github.com>
2022-12-06 18:35:02 -0600

    Update hotel-review-rome-code.py

--- hotel-review-rome-code.py
+++ hotel-review-rome-code.py
@@ -192,7 +192,7 @@ with features:
     for query in queries:
         st.write(queries)
 
-        query_embedding = model.encode(query)#, convert_to_tensor=True)
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
 
     # We use cosine-similarity and torch.topk to find the highest 5 scores
         cos_scores = util.pytorch_cos_sim(query_embedding, embeddings)[0]
