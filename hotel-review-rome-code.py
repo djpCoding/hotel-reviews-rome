e9a625a9832096e59cf9e5d1e2e7786d76e467eb
djpCoding <119716129+djpCoding@users.noreply.github.com>
2022-12-06 19:09:52 -0600

    Update hotel-review-rome-code.py

--- hotel-review-rome-code.py
+++ hotel-review-rome-code.py
@@ -33,8 +33,8 @@ import torchvision
 import re
 import numpy as np
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('punkt')
 import heapq
 import matplotlib
 import matplotlib.pyplot as plt
